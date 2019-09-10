287ff05e7404c582400019cf74258fbe211cc6e7
Dou Jones <misterchiefexecutiveofficer@yahoo.com>
2019-09-10 18:51:16 -0400

    second commit

--- components/InfoBox.js
+++ components/InfoBox.js
@@ -5,16 +5,20 @@ class InfoBox extends Component{
     words: ''
   }
   
+  onWillSubmit = (e) => {
+    e.preventDefault();
+  }
+
   render(){
   return (
   <div className="ui segment">
-  <form className="ui form">
+  <form className="ui form" onSubmit={this.onWillSubmit}>
   <div className="field">
   <label>Picture Search</label>
   <input
   type="text"
   value={this.state.words}
-  onChange={(e) => this.setState({words: e.target.value})}
+  onChange={(e) => this.setState({words: e.target.value.toLowerCase()})}
   />
   </div>
   </form>
