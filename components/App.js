6e5ee56bd1d4b480fe86b63faf37e77da112ae3c
Dou Jones <misterchiefexecutiveofficer@yahoo.com>
2019-09-17 21:20:19 -0400

    commit

--- components/App.js
+++ components/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import InfoBox from './InfoBox'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header'
+import Searchbar from '../components/Boxes/boxWrapper'
+import Hero from './Hero';
+
 
 const App = () => {
   return (
-    <div className="ui container" style={{ marginTop: '15px' }}>
-  <InfoBox/>
-  </div>
+    <>
+    <p style={{marginLeft: "10%", marginTop: ".9em"}} className="para1">
+    1) User logs into the Home Page
+    </p>
+    <Header/>
+    <p style={{marginLeft: "12%", marginBottom: ".4em"}} className="para1">
+    2) User clicks on Create New Task link
+    </p>
+    <Searchbar/>
+    <Hero/>
+  </>
+ 
+ 
 )
 }
 
--- components/InfoBox.js
+++ components/InfoBox.js
@@ -1,31 +1,14 @@
 import React, { Component } from 'react'
 
-class InfoBox extends Component{
-  state= {
-    words: ''
+export default class InfoBox extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
   }
-  
-  onWillSubmit = (e) => {
-    e.preventDefault();
-  }
-
-  render(){
-  return (
-  <div className="ui segment">
-  <form className="ui form" onSubmit={this.onWillSubmit}>
-  <div className="field">
-  <label>Picture Search</label>
-  <input
-  type="text"
-  value={this.state.words}
-  onChange={(e) => this.setState({words: e.target.value.toLowerCase()})}
-  />
-  </div>
-  </form>
-  </div>
-)
 }
-}
-export default InfoBox
 
-// Add Semantic UI by adding the Minified Version CSS
+
+
